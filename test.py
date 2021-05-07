723e2ec307db859c2ef9ff84c5eb55d0d4c91f0d
Anthony Giove <tony@innocu.us>
2021-05-06 22:18:51 -0400

    changes

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('tony rocks')
+import re
+import pandas as pd
+import numpy as np
+
+# df = pd.read_html('https://www.capfriendly.com/teams/redwings/cap-tracker/2019')
+# print(df)
+
+import requests
+url = 'https://www.capfriendly.com/teams/redwings/cap-tracker/2019'
+
+r = requests.get(url)
+
+print (r.content)
